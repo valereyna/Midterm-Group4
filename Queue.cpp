68542ae1963b00a45fb5979d142020c2055824ee
Earthree72 <125017576+Earthree72@users.noreply.github.com>
2023-10-09 22:22:45 +0700

    Update Queue.cpp
    Adding main code for Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,83 @@
+//call the Queue.h
+#include "Queue.h"
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+Queue::Queue(int size)
+{
+	front = rear = 0;
+	this->size = size;
+	array = new double[size];
+	if (array == nullptr)
+	{
+		cout << "Error" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+Queue::~Queue()
+{
+	delete[] array;
+}
+
+bool Queue::Full()
+{
+	return (size == rear);
+}
+
+bool Queue::Empty()
+{
+	return (front == rear);
+}
+
+void Queue::Enqueue(double K)
+{
+	if (Full())
+	{
+		cout << "\n\tOverflow" << endl;
+		return;
+	}
+	array[rear] = K;
+	rear++;
+}
+
+void Queue::characterEnqueue(char C)
+{
+	if (Full())
+	{
+		cout << "\n\tOverflow" << endl;
+		return;
+	}
+	array[rear] = C;
+	rear++;
+}
+
+void Queue::Dequeue()
+{
+	if (Empty())
+	{
+		cout << "\n\tUnderflow" << endl;
+		return;
+	}
+	rear--;
+}
+
+double Queue::getFront()
+{
+	if (Empty())
+	{
+		cout << "\n\tUnderflow" << endl;
+	}
+	return array[front];
+}
+
+double Queue::getRear()
+{
+	if (Empty())
+	{
+		cout << "\n\tUnderflow" << endl;
+	}
+	return array[rear - 1];
+}
